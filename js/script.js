1032b9a479d2345fba71e2e6581c3c44724f3e44
Ditechgit <diogo.jesus.tech@outlook.com>
2024-12-27 21:30:43 -0300

    Estilizaçao - 5
    Alteração de conteudo principal

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ function hideSidebar(){
     const sidebar = document.querySelector(".vertical-menu")
     sidebar.style.display = "none"
 }
+
+/* Parte do Modal - Dialog */
+
+const button = document.querySelector(".modal-btn");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog span")
+
+button.onclick = function(){
+    modal.showModal()
+}
+buttonClose.onclick = function() {
+    modal.close()
+}
+
+
+
