646d9ca5f5d21426b3cfcbb85caee0e954ef77e8
Ditechgit <diogo.jesus.tech@outlook.com>
2024-12-28 17:54:55 -0300

    Alteração e Inclusão
    Inclui o site Amellie na prévia e tirei o modal

--- js/script.js
+++ js/script.js
@@ -7,18 +7,3 @@ function hideSidebar(){
     sidebar.style.display = "none"
 }
 
-/* Parte do Modal - Dialog */
-
-const button = document.querySelector(".modal-btn");
-const modal = document.querySelector("dialog");
-const buttonClose = document.querySelector("dialog span")
-
-button.onclick = function(){
-    modal.showModal()
-}
-buttonClose.onclick = function() {
-    modal.close()
-}
-
-
-
