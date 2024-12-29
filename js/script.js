bf0e907c43787bb54ba61f566bdc13c594dbcb4e
Ditechgit <diogo.jesus.tech@outlook.com>
2024-12-29 18:36:41 -0300

    Função Botão

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ function hideSidebar(){
     sidebar.style.display = "none"
 }
 
+const scrollToTopButton = document.querySelector("#scrollToTop");
+
+// Função para mostrar ou esconder o botão baseado na posição de rolagem
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+});
+
+// Função para voltar ao topo
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
